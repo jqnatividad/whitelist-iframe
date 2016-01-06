47804fac9bc4250b84c55dbda0bae51754988d6a
jqnatividad <joel.natividad@ontodia.com>
2016-01-06 14:19:00 -0500

    added doodle, civicdashboards, data.beta.nyc

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -2,5 +2,8 @@
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/doodle\.com\/poll\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/civicdashboards\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/data\.beta\.nyc\/.+/i);
 })();
 
